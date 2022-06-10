18433ed9f4e6123e17d832c18de3c29ee9f00cd6
franrodriguez1993 <francojmprogramacion@gmail.com>
2022-06-10 10:57:11 -0300

    modificaciones de fecha de nacimiento

--- familiaresApp/admin.py
+++ familiaresApp/admin.py
@@ -3,7 +3,7 @@ from familiaresApp.models import Familiares
 # Register your models here.
 
 class FamiliaresAdmin(admin.ModelAdmin):
-    list_display = ('nombre','apellido','edad','email')
+    list_display = ('nombre','apellido','nacimiento','edad','email')
 
 admin.site.register(Familiares,FamiliaresAdmin)
     
--- familiaresApp/models.py
+++ familiaresApp/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Familiares(models.Model):
     nombre = models.CharField(max_length=15)
     apellido = models.CharField(max_length=15)
+    nacimiento = models.DateTimeField()
     edad = models.IntegerField()
     email = models.EmailField()
+    
     
