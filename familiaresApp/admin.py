e90d65bf0390237ecf32732eb80c60d9606e510d
franrodriguez1993 <francojmprogramacion@gmail.com>
2022-06-10 10:38:27 -0300

    template listo

--- familiaresApp/admin.py
+++ familiaresApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from familiaresApp.models import Familiares
 # Register your models here.
+
+class FamiliaresAdmin(admin.ModelAdmin):
+    list_display = ('nombre','apellido','edad','email')
+
+admin.site.register(Familiares,FamiliaresAdmin)
+    
--- familiaresApp/views.py
+++ familiaresApp/views.py
@@ -2,11 +2,16 @@ from contextvars import Context
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.template import Template,context
+
+from familiaresApp.models import Familiares
 # Create your views here.
 
 
 
 def inicio(request):
-    lista = [1,2,4,5,6]
-    return render(request,'index.html',{"lista": lista})
     
+    familiares = Familiares.objects.all()
+
+    return render(request,'index.html',{"familiares":familiares})
+
+
